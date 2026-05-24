7c58ed6fcfed9291ec7ac412fbefb83954b93d32
Alex Alvarado <alex.alvarado.rae@gmail.com>
2026-05-23 20:23:22 -0500

    feat: Update CreatePostRequest to require content length validation

--- Application/Models/Requests/Post/CreatePostRequest.cs
+++ Application/Models/Requests/Post/CreatePostRequest.cs
@@ -5,9 +5,7 @@ namespace Application.Models.Requests.Post;
 
 public class CreatePostRequest
 {
-    [Required(ErrorMessage = ValidationConstants.REQUIRED)]
     [MaxLength(500, ErrorMessage = ValidationConstants.MAX_LENGTH)]
-    [MinLength(3, ErrorMessage = ValidationConstants.MIN_LENGTH)]
     public string Content { get; set; } = null!;
 
     public bool? IsPublished { get; set; }
